1e443f097a73bfae50dedfc8103501f9e6a7d290
Stian <01stisor@gmail.com>
2022-10-13 13:26:29 +0200

    små ting

---
+++ src/main/java/com/example/oblig2/View/BTView.java
@@ -0,0 +1,15 @@
+package com.example.oblig2.View;
+
+import com.example.oblig2.Model.BST;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+
+public class BTView extends Pane {
+    private BST<Integer> tree = new BST<>();
+    
+    
+}
