fe61bc3a7445ce97dbbb6b03c80b07b55423c06f
JesperHagen <56399627+JesperHagen@users.noreply.github.com>
2022-10-13 12:41:41 +0200

    tree.java

--- src/main/java/com/example/oblig2/Model/Tree.java
+++ src/main/java/com/example/oblig2/Model/Tree.java
@@ -1,5 +1,116 @@
 package com.example.oblig2.Model;
 
-public class Tree {
-    
+import java.util.Collection;
+import java.util.Iterator;
+
+public interface Tree<E> extends Collection {
+
+    public boolean search(E e);
+
+    public boolean insert(E e);
+
+    public boolean delete(E e);
+
+    public int getsize();
+
+    public default void inorder() {
+
+    }
+
+    public default void postorder() {
+
+    }
+
+    public default preorder() {
+
+    }
+
+    @Override
+    public default boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public default boolean contains(Object e){
+       return search((E)e);
+    }
+
+    @Override
+    public default boolean add(E e) {
+        return insert(e);
+    }
+
+    @Override
+    public default boolean remove(Object e) {
+        return delete((E)e);
+    }
+
+    public default int size() {
+        return getsize();
+    }
+
+    @Override
+    public default boolean containsAll(Collection<?> c){
+        for (Object e : c)
+            if (!contains(e))
+                return false;
+        return true;
+    }
+
+    @Override
+    public default boolean addAll(Collection<? extends E> c) {
+        for(Object e : c)
+            add((E) c);
+        return true;
+    }
+
+    @Override
+    public default boolean removeAll(Collection<?> c) {
+        try {
+            for (int i = 0; i <= c.size(); i++) {
+                remove(i);
+            }
+            return true;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public default boolean retainAll(Collection<?> c) {
+        if (c == null)
+            throw new NullPointerException("the collection is null");
+
+        Iterator itr = iterator();
+
+        boolean found = false;
+        while(itr.hasNext()){
+            if (!c.contains(itr.next()))
+                itr.remove();
+            found = true;
+        }
+        return found;
+    }
+
+    @Override
+    public default Object[] toArray() {
+
+        int indexCount = 0;
+        Iterator<E> itr = iterator();
+
+        while(itr.hasNext()) {
+            indexCount++;
+        }
+        Object[] arr = new Object[indexCount];
+
+        while(itr.hasNext()){
+            arr[indexCount] = itr.next();
+        }
+        return arr;
+    }
+
+    @Override
+    public default <T> T[] toArray(T[] array) {
+
+    }
 }
