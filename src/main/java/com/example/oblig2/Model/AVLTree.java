e101e59fd3c7e3c4743680779a82296bd740e7dd
JesperHagen <56399627+JesperHagen@users.noreply.github.com>
2022-10-19 12:42:26 +0200

    AVLTree (med feilmeldinger)

--- src/main/java/com/example/oblig2/Model/AVLTree.java
+++ src/main/java/com/example/oblig2/Model/AVLTree.java
@@ -32,20 +32,20 @@ public class AVLTree<E> extends BST<E> {
     private void updateHeight(AVLTreeNode<E> node) {
         if (node.left == null && node.right == null)
             node.height = 0;
-        else if (node.left == 0)
+        else if (node.left == null)
             node.height = 1 + ((AVLTreeNode<E>)(node.right)).height;
         else if (node.right == null)
             node.height = 1 + ((AVLTreeNode<E>)(node.left)).height;
         else
             node.height = 1 +
-                    Math.max(((AVLtreeNode<E>)(node.right)).height,
+                    Math.max(((AVLTreeNode<E>)(node.right)).height,
                             ((AVLTreeNode<E>)(node.left)).height);
 
     }
 
     private void balancePath(E e) {
         java.util.ArrayList<TreeNode<E>> path = path(e);
-        for (int i = path.size() - 1; i >= 0; i--)
+        for (int i = path.size() - 1; i >= 0; i--) {
             AVLTreeNode<E> A = (AVLTreeNode<E>)(path.get(i));
             updateHeight(A);
             AVLTreeNode<E> parentOfA = (A == root) ? null :
@@ -53,7 +53,7 @@ public class AVLTree<E> extends BST<E> {
 
             switch (balanceFactor(A)) {
                 case - 2:
-                    if (balanceFactor ((AVLTreNode<E>)A.left) <= 0) {
+                    if (balanceFactor ((AVLTreeNode<E>)A.left) <= 0) {
                         balanceLL(A, parentOfA);
                     }
                     else {
@@ -72,3 +72,194 @@ public class AVLTree<E> extends BST<E> {
     }
 
 }
+
+private int balanceFactor (AVLTree<E> node) {
+    if (node.right == null)
+        return -node.height;
+    else if (node.left == null)
+        return +node.height;
+    else
+        return ((AVLTree<E>)node.right).height -
+                ((AVLTree<E>)node.left)height;
+}
+
+    private void balanceLL(TreeNode<E> A, TreeNode<E> parentOfA) {
+        TreeNode<E> B = A.left; // A is left-heavy and B is left-heavy
+
+        if (A == root) {
+            root = B;
+        }
+        else {
+            if (parentOfA.left == A) {
+                parentOfA.left = B;
+            }
+            else {
+                parentOfA.right = B;
+            }
+        }
+
+        A.left = B.right;
+        B.right = A;
+        updateHeight((AVLTreeNode<E>)A);
+        updateHeight((AVLTreeNode<E>)B);
+    }
+
+    private void balanceLR(TreeNode<E> A, TreeNode<E> parentOfA) {
+        TreeNode<E> B = A.left; // A is left-heavy
+        TreeNode<E> C = B.right; // B is right-heavy
+
+        if (A == root) {
+            root = C;
+        }
+        else {
+            if (parentOfA.left == A) {
+                parentOfA.left = C;
+            }
+            else {
+                parentOfA.right = C;
+            }
+        }
+
+        A.left = C.right; // Make T3 the left subtree of A
+        B.right = C.left; // Make T2 the right subtree of B
+        C.left = B;
+        C.right = A;
+
+        // Adjust heights
+        updateHeight((AVLTreeNode<E>)A);
+        updateHeight((AVLTreeNode<E>)B);
+        updateHeight((AVLTreeNode<E>)C);
+    }
+
+    private void balanceRR(TreeNode<E> A, TreeNode<E> parentOfA) {
+        TreeNode<E> B = A.right;
+
+        if (A == root) {
+            root = B;
+        }
+        else {
+            if (parentOfA.left == A) {
+                parentOfA.left = B;
+            }
+            else {
+                parentOfA.right = B;
+            }
+        }
+
+        A.right = B.left;
+        B.left = A;
+        updateHeight((AVLTreeNode<E>)A);
+        updateHeight((AVLTreeNode<E>)B);
+    }
+
+    private void balanceRL(TreeNode<E> A, TreeNode<E> parentOfA) {
+        TreeNode<E> B = A.right; // A is right-heavy
+        TreeNode<E> C = B.left; // B is left-heavy
+
+        if (A == root) {
+            root = C;
+        }
+        else {
+            if (parentOfA.left == A) {
+                parentOfA.left = C;
+            }
+            else {
+                parentOfA.right = C;
+            }
+        }
+
+        A.right = C.left; // Make T2 the right subtree of A
+        B.left = C.right; // Make T3 the left subtree of B
+        C.left = A;
+        C.right = B;
+
+        // Adjust heights
+        updateHeight((AVLTreeNode<E>)A);
+        updateHeight((AVLTreeNode<E>)B);
+        updateHeight((AVLTreeNode<E>)C);
+    }
+
+    @Override /** Delete an element from the binary tree.
+     * Return true if the element is deleted successfully
+     * Return false if the element is not in the tree */
+    public boolean delete(E element) {
+        if (root == null)
+            return false; // Element is not in the tree
+
+        // Locate the node to be deleted and also locate its parent node
+        TreeNode<E> parent = null;
+        TreeNode<E> current = root;
+        while (current != null) {
+            if (c.compare(element, current.element) < 0) {
+                parent = current;
+                current = current.left;
+            }
+            else if (c.compare(element, current.element) > 0) {
+                parent = current;
+                current = current.right;
+            }
+            else
+                break; // Element is in the tree pointed by current
+        }
+
+        if (current == null)
+            return false; // Element is not in the tree
+
+        // Case 1: current has no left children (See Figure 23.6)
+        if (current.left == null) {
+            // Connect the parent with the right child of the current node
+            if (parent == null) {
+                root = current.right;
+            }
+            else {
+                if (c.compare(element, parent.element) < 0)
+                    parent.left = current.right;
+                else
+                    parent.right = current.right;
+
+                // Balance the tree if necessary
+                balancePath(parent.element);
+            }
+        }
+        else {
+            // Case 2: The current node has a left child
+            // Locate the rightmost node in the left subtree of
+            // the current node and also its parent
+            TreeNode<E> parentOfRightMost = current;
+            TreeNode<E> rightMost = current.left;
+
+            while (rightMost.right != null) {
+                parentOfRightMost = rightMost;
+                rightMost = rightMost.right; // Keep going to the right
+            }
+
+            // Replace the element in current by the element in rightMost
+            current.element = rightMost.element;
+
+            // Eliminate rightmost node
+            if (parentOfRightMost.right == rightMost)
+                parentOfRightMost.right = rightMost.left;
+            else
+                // Special case: parentOfRightMost is current
+                parentOfRightMost.left = rightMost.left;
+
+            // Balance the tree if necessary
+            balancePath(parentOfRightMost.element);
+        }
+
+        size--;
+        return true; // Element inserted
+    }
+}
+
+protected static class AVLTreeNode<E> extends BST.TreeNode<E> {
+    protected int height = 0; // New data field
+
+    public AVLTreeNode(E o) {
+        super(o);
+    }
+}
+
+
+
+
