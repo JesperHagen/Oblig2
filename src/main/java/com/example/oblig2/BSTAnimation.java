32cd22cec911311fc0029525a5203e53f231ef8b
Stian <01stisor@gmail.com>
2022-10-26 10:26:10 +0200

    lagt til btRandom og Btsearch

--- src/main/java/com/example/oblig2/BSTAnimation.java
+++ src/main/java/com/example/oblig2/BSTAnimation.java
@@ -1,7 +1,7 @@
 package com.example.oblig2;
 import com.example.oblig2.Model.*;
 import com.example.oblig2.View.*;
-import com.example.oblig2.*;
+import com.example.oblig2.Model.AVLTree;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
@@ -16,51 +16,79 @@ import javafx.scene.layout.HBox;
 public class BSTAnimation extends Application {
     @Override
     public void start(Stage primaryStage) {
-        BST<Integer> tree = new BST<>();
-
         BorderPane pane = new BorderPane();
-        BTView view = new BTView(tree);
         
-        pane.setCenter(view);
+        
+        AVLTree<Integer> avlTree = new AVLTree<>();
+        BTView avlView = new BTView(avlTree);
+
+        pane.setCenter(avlView);
         
         TextField tfKey = new TextField();
         tfKey.setPrefColumnCount(3);
         tfKey.setAlignment(Pos.BASELINE_RIGHT);
-        Button btInsert = new Button("Insert");
-        Button btDelete = new Button("Delete");
+        Button btInsert = new Button("Sett inn");
+        Button btDelete = new Button("Slett");
+        Button btSearch = new Button("Søk");
+        //Legg til 10 tilfeldige verdier
+        Button btRandom = new Button("Legg til 10 tilfeldige verdier");
+
+        
+
         HBox hBox = new HBox(5);
-        hBox.getChildren().addAll(new Label("Enter a key: "), tfKey, btInsert, btDelete);
+        hBox.getChildren().addAll(new Label("Enter a key: "), tfKey, btInsert, btDelete, btSearch, btRandom);
         hBox.setAlignment(Pos.CENTER);
         pane.setBottom(hBox);
 
         btInsert.setOnAction(e -> {
             int key = Integer.parseInt(tfKey.getText());
-            if (tree.search(key)) { // key is in the tree already
-                view.displayTree();
-                view.setStatus(key + " is already in the tree");
+            if (avlTree.search(key)) { // key is in the avlTree already
+                avlView.displayTree();
+                avlView.setStatus(key + " er allerede i treet");
             } else {
-                tree.insert(key); // Insert a new key
-                view.displayTree();
-                view.setStatus(key + " is inserted in the tree");
+                avlTree.insert(key); // Insert a new key
+                avlView.displayTree();
+                avlView.setStatus(key + " er satt inn i treet");
             }
-        });
-
+        });  
         btDelete.setOnAction(e -> {
             int key = Integer.parseInt(tfKey.getText());
-            if (!tree.search(key)) { // key is not in the tree
-                view.displayTree();
-                view.setStatus(key + " is not in the tree");
+            if (!avlTree.search(key)) { // key is not in the avlTree
+                avlView.displayTree();
+                avlView.setStatus(key + " er ikke i treet");
             } else {
-                tree.delete(key); // Delete a key
-                view.displayTree();
-                view.setStatus(key + " is deleted from the tree");
+                avlTree.delete(key); // Delete a key
+                avlView.displayTree();
+                avlView.setStatus(key + " er slettet fra treet");
             }
         });
-
-
+        btSearch.setOnAction(e -> {
+            int key = Integer.parseInt(tfKey.getText());
+            if (!avlTree.search(key)) { // key is not in the avlTree
+                avlView.displayTree();
+                avlView.setStatus(key + " er ikke i treet");
+            } else {
+                avlView.displayTree();
+                avlView.setStatus(key + " er i treet");
+            }
+        });
+        btRandom.setOnAction(e -> {
+            for (int i = 0; i < 10; i++) {
+                int key = (int)(Math.random() * 100);
+                if (avlTree.search(key)) { // key is in the avlTree already
+                    avlView.displayTree();
+                    avlView.setStatus(key + " er allerede i treet");
+                } else {
+                    avlTree.insert(key); // Insert a new key
+                    avlView.displayTree();
+                    avlView.setStatus(key + " er satt inn i treet");
+                }
+            }
+        });
+     
         // Create a scene and place it in the stage
-        Scene scene = new Scene(pane, 800, 800);
-        primaryStage.setTitle("BSTAnimation"); // Set the stage title
+        Scene scene = new Scene(pane, 800, 600);
+        primaryStage.setTitle("AVL Tree"); // Set the stage title
         primaryStage.setScene(scene); // Place the scene in the stage
         primaryStage.show(); // Display the stage
     }    
